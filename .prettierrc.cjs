d75780f1947e6b3229f6a0308f88a5ea9badb1c9
Santiago Galindo <sp.galindoh@gmail.com>
2024-07-13 10:42:23 -0500

    ESLint, Prettier and Tailwind Setup

---
+++ .prettierrc.cjs
@@ -0,0 +1,6 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 120,
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [{ files: "*.astro", options: { parser: "astro" } }],
+};
