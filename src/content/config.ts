7d8f56ba3754457682b1a6fe6b7be059dc64e05b
Santiago Galindo <sp.galindoh@gmail.com>
2024-08-08 18:13:46 -0500

    Zod Image Type and Viafoura Conversations Count

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,12 +2,14 @@ import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   type: "content",
-  schema: z.object({
-    image: z.string(),
-    title: z.string(),
-    excerpt: z.string(),
-    author: z.string(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      vfContainerId: z.number(),
+      image: image(),
+      title: z.string(),
+      excerpt: z.string(),
+      author: z.string(),
+    }),
 });
 
 export const collections = { blog };
