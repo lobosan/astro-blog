5830eb37cc03697b65aa40565c0a38d016f73cdb
Santiago Galindo <sp.galindoh@gmail.com>
2024-08-12 13:32:44 -0500

    Inter Font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        inter: ["Inter", ...defaultTheme.fontFamily.sans],
       },
       screens: {
         sm: "640px",
