49a30748278506fe4f9af3d81386c706c11528f5
Santiago Galindo <sp.galindoh@gmail.com>
2024-08-16 12:14:04 -0500

    Sentry Tracing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import sentry from "@sentry/astro";
 
 export default defineConfig({
   build: {
@@ -8,6 +9,13 @@ export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false,
+    }),
+    sentry({
+      dsn: "https://658ce450b87f680796e13c1c0bc1440e@o4507788070617088.ingest.us.sentry.io/4507788071927808",
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
     }),
   ],
 });
