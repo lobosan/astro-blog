38a3aaa31b7787269f64e73d202269def4c380a3
Santiago Galindo <sp.galindoh@gmail.com>
2024-08-20 13:14:21 -0500

    Remove Sentry and Change Blog to Posts Collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,8 @@
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import sentry from "@sentry/astro";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   build: { inlineStylesheets: "always" },
-  integrations: [
-    mdx(),
-    tailwind({ applyBaseStyles: false }),
-    sentry({
-      dsn: "https://658ce450b87f680796e13c1c0bc1440e@o4507788070617088.ingest.us.sentry.io/4507788071927808",
-      replaysSessionSampleRate: 0,
-      replaysOnErrorSampleRate: 0,
-      sourceMapsUploadOptions: {
-        project: "javascript-astro",
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-      },
-    }),
-  ],
+  integrations: [mdx(), tailwind({ applyBaseStyles: false })],
 });
