051dc0f42e7ed2b5dc3520df68efb1a63d7c93c7
Santiago Galindo <sp.galindoh@gmail.com>
2024-08-22 16:18:15 -0500

    Remove Tailwind Forms

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,8 @@
-import formsPlugin from "@tailwindcss/forms";
 import typographyPlugin from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
-  plugins: [typographyPlugin, formsPlugin],
+  plugins: [typographyPlugin],
   darkMode: "class",
 };
