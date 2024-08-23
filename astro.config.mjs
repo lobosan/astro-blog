dd6df566c5cbaad9256b35f274476311678b1162
Santiago Galindo <sp.galindoh@gmail.com>
2024-08-23 16:08:55 -0500

    Viafoura Min-Heights

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
+import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  build: { inlineStylesheets: "always" },
-  integrations: [tailwind({ applyBaseStyles: false })],
+  build: {
+    inlineStylesheets: "always",
+  },
+  integrations: [
+    mdx(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
