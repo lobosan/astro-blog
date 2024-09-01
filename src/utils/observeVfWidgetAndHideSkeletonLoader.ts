6785c2dc06b2ddab8189599bc4c597f5f475083a
Santiago Galindo <sp.galindoh@gmail.com>
2024-08-31 21:10:54 -0500

    Custom Skeleton

---
+++ src/utils/observeVfWidgetAndHideSkeletonLoader.ts
@@ -0,0 +1,16 @@
+export const observeVfWidgetAndHideSkeletonLoader = (targetSelector: string) => {
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      mutation.addedNodes.forEach(node => {
+        if (node instanceof HTMLElement) {
+          const target = node.matches(targetSelector) ? node : node.querySelector(targetSelector);
+          if (target) {
+            const skeletonLoader = target.closest(".viafoura")?.querySelector(".skeleton-loader");
+            skeletonLoader?.classList.add("hidden");
+          }
+        }
+      });
+    });
+  });
+  observer.observe(document.body, { childList: true, subtree: true });
+};
