159e9c38aa7a565a0cceb90f684664478eb2fc9b
Santiago Galindo <sp.galindoh@gmail.com>
2024-09-04 12:40:44 -0500

    Biome Setup

--- .prettierrc.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('prettier').Config} */
-export default {
-  printWidth: 100,
-  arrowParens: "avoid",
-  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-  overrides: [{ files: "*.astro", options: { parser: "astro" } }],
-};
