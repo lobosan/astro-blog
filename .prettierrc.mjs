b28109c1d382a0902c69e89d2db3584043c469c8
Santiago Galindo <sp.galindoh@gmail.com>
2024-09-07 13:05:36 -0500

    Preload Font and Shimmer Animation

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,6 @@
 /** @type {import('prettier').Config} */
 export default {
-  printWidth: 100,
+  printWidth: 120,
   arrowParens: "avoid",
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [{ files: "*.astro", options: { parser: "astro" } }],
